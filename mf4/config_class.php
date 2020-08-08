e02aff34a669831b1bcbaaa9589c955d58463c3e
Ian Maclagan <iwmaclagan@gmail.com>
2020-08-08 17:42:04 +1000

    removed whitespace at end of config class file that was causing session to not be able to start

--- mf4/config_class.php
+++ mf4/config_class.php
@@ -1,48 +1,93 @@
-<?php
-  class Config {
-  
-        private static  $instance = NULL;
-        protected  $__data = array(); 
-    
-        public static function singleton() {
-            if (self::$instance == NULL) {          
-                self::$instance = new Config(); 
-            } 
-                return self::$instance;   
-        }
-        
-        // make PRIVATE to stop anyone using NEW or CLONE
-        private function __construct() {}
-        private function __clone(){}
-        
-        public function __get($property) {
-            if (isSet($this->__data[$property])) {
-                return $this->__data[$property];
-            } else {
-                return false;
-            }    
-        }
-        
-        public function __set($property,$value) {
-            $this->__data[$property] = $value;
-        }
-        
-        public  function __isset($property){
-            if (isSet($this->__data[$property])){
-            return true;
-            } else {
-                return false;
-            }
-        }
- 
-        public  function __unset($property){
-        
-            if ( isSet($this->__data[$property]) ) {
-                unset( $this->__data[$property] );
-                return true;
-            } else {
-                return false;
-            }
-        }       
-  }
-?>
+<?php
+
+  class Config {
+
+  
+
+        private static  $instance = NULL;
+
+        protected  $__data = array(); 
+
+    
+
+        public static function singleton() {
+
+            if (self::$instance == NULL) {          
+
+                self::$instance = new Config(); 
+
+            } 
+
+                return self::$instance;   
+
+        }
+
+        
+
+        // make PRIVATE to stop anyone using NEW or CLONE
+
+        private function __construct() {}
+
+        private function __clone(){}
+
+        
+
+        public function __get($property) {
+
+            if (isSet($this->__data[$property])) {
+
+                return $this->__data[$property];
+
+            } else {
+
+                return false;
+
+            }    
+
+        }
+
+        
+
+        public function __set($property,$value) {
+
+            $this->__data[$property] = $value;
+
+        }
+
+        
+
+        public  function __isset($property){
+
+            if (isSet($this->__data[$property])){
+
+            return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        }
+
+ 
+
+        public  function __unset($property){
+
+        
+
+            if ( isSet($this->__data[$property]) ) {
+
+                unset( $this->__data[$property] );
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        }       
+
+  }
